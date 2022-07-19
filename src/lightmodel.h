1d077874d36dd918d8e869e733431a1e7fcb17f9
Zack Cheng <61947377+zackchengyk@users.noreply.github.com>
2022-07-18 20:19:40 -0400

    Update lightmodel.h

--- src/lightmodel.h
+++ src/lightmodel.h
@@ -8,10 +8,12 @@
 
 #include <vector>
 
-
-
-// position, normal, material, lightinfo
-RGBA phong(glm::vec3 p, glm::vec3 n, glm::vec3 c, Material m, std::vector<LightInfo> lights, Sampler refl_sampler);
-
+RGBA phong(
+  glm::vec3 position,
+  glm::vec3 normal,
+  glm::vec3 directionToCamera,
+  Material material,
+  std::vector<LightInfo> lights,
+  Sampler reflectionSampler);
 
 #endif // LIGHTMODEL_H
